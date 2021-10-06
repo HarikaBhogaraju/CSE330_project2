1da991f523964a8cbe74cb744521be89943b574a
HarikaBhogaraju <harika.bhogaraju@gmail.com>
2021-10-05 19:49:35 -0700

    final changes

--- thread_test.c
+++ thread_test.c
@@ -8,49 +8,27 @@ int y = 0;
 int glob = 1;
 int loc = 0;
 
-
-void runTest1(){
-	int loc = 0;
-		while(1){
-			loc++;
-			printf("This is %d th execution of thread 1 with global var value % d\n", loc, glob);
-			glob++;
-			sleep(1);
-			yield();
-		}
-}
-
-void runTest2(){
-	int loc = 0;
-		while(1){
-			loc++;
-			printf("This is %d th execution of thread 2 with global var value % d\n", loc, glob);
-			glob++;
-			sleep(1);
-			yield();
-		}
-}
-
 void read_ints (const char* file_name)
 {
   FILE* file = fopen (file_name, "r");
-  fscanf(file,"%d,%d",&x,&y); 
-  fclose (file);        
+  fscanf(file,"%d,%d",&x,&y);
+  fclose (file);
 }
+
 void runTest(){
 	for(int i = 1;i<=y;i++){
 			loc = loc + glob;
 			printf("\n");
-                   	printf("This is %d th execution of thread %d  with global var value % d\n", i,glob,loc);
+      printf("This is %d th execution of thread %d with global var value %d\n", i,glob,loc);
 			if(glob == x){
 				glob = 1;
 			}
 			else{
 				glob++;
 			}
-                        sleep(1);
-                        yield();
-	}	
+      sleep(1);
+      yield();
+	}
 }
 
 int main(int argc, char** argv){
@@ -62,17 +40,15 @@ int main(int argc, char** argv){
 		return 0;
 	}
 	else{
-	
-	struct TCB_t* threads[x];
-        
-        InitQueue(&runQ);
-        
-	for(int i = 0;i<x;i++){
-		startThread(runTest);
-	}
 
-	run();
-	return 0;
+	  InitQueue(&runQ);
+
+		for(int i = 0;i<x;i++){
+			startThread(runTest);
+		}
+
+		run();
+		return 0;
 	}
 
 }
